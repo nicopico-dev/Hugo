a2ddca6b3dfcd978700a915b53b13ecf564ebc64
Nicolas Picon <nicopico.dev@gmail.com>
2018-01-16 20:17:28 +0100

    Coroutines utilities

---
+++ app/src/main/java/fr/nicopico/hugo/utils/coroutines.kt
@@ -0,0 +1,39 @@
+package fr.nicopico.hugo.utils
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.UI
+
+/**
+ * @see https://medium.com/@erikhellman/simple-asynchronous-loading-with-kotlin-coroutines-f26408f97f46
+ */
+
+class CoroutineLifecycleListener(private val deferred: Deferred<*>) : LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun cancelCoroutine() {
+        if (!deferred.isCancelled) {
+            deferred.cancel()
+        }
+    }
+}
+
+@Suppress("PropertyName")
+internal val Background = newFixedThreadPoolContext(2, "bg")
+
+fun <T> LifecycleOwner.load(loader: () -> T): Deferred<T> {
+    val deferred = async(context = Background, start = CoroutineStart.LAZY) {
+        loader()
+    }
+
+    lifecycle.addObserver(CoroutineLifecycleListener(deferred))
+    return deferred
+}
+
+infix fun <T> Deferred<T>.then(block: (T) -> Unit): Job {
+    return launch(context = UI) {
+        block(this@then.await())
+    }
+}
