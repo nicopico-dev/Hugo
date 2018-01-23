447a1f856fbed2f350a511e6d00dabf7a6717e89
Nicolas Picon <nicopico.dev@gmail.com>
2018-01-23 22:54:56 +0100

    Add OpenSources licenses

--- app/src/main/java/fr/nicopico/hugo/ui/main/MainActivity.kt
+++ app/src/main/java/fr/nicopico/hugo/ui/main/MainActivity.kt
@@ -1,9 +1,14 @@
 package fr.nicopico.hugo.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import fr.nicopico.hugo.R
 import fr.nicopico.hugo.ui.BaseActivity
 import fr.nicopico.hugo.ui.timeline.TimelineFragment
+
 
 class MainActivity : BaseActivity() {
 
@@ -17,5 +22,19 @@ class MainActivity : BaseActivity() {
                     .replace(R.id.container, TimelineFragment())
                     .commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_licenses) {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
