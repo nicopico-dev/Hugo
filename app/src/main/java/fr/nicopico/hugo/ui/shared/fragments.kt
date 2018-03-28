a11f71b8b5bf24a92dd9879d7969ef71cc0b45ae
Nicolas Picon <nicopico.dev@gmail.com>
2018-03-28 22:39:55 +0200

    Replace fragments.kt bundle() with KTX bundleOf()

--- app/src/main/java/fr/nicopico/hugo/ui/shared/fragments.kt
+++ app/src/main/java/fr/nicopico/hugo/ui/shared/fragments.kt
@@ -1,30 +1,12 @@
 package fr.nicopico.hugo.ui.shared
 
-import android.os.Bundle
-import android.os.Parcelable
 import android.support.v4.app.Fragment
-import java.io.Serializable
+import androidx.os.bundleOf
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-@Suppress("ComplexMethod")
-fun bundle(vararg args: Pair<String, Any?>) = Bundle().apply {
-    args.forEach { (key, value) ->
-        when (value) {
-            is String -> putString(key, value)
-            is CharSequence -> putCharSequence(key, value)
-            is Int -> putInt(key, value)
-            is Long -> putLong(key, value)
-            is Bundle -> putBundle(key, value)
-            is Parcelable -> putParcelable(key, value)
-            is Serializable -> putSerializable(key, value)
-            else -> throw UnsupportedOperationException("type of $value is not supported")
-        }
-    }
-}
-
 fun <T> T.withArguments(vararg args: Pair<String, Any?>): T where T : Fragment {
-    arguments = bundle(*args)
+    arguments = bundleOf(*args)
     return this
 }
 
