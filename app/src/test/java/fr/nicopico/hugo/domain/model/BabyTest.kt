bb11a0c334bf4822f9e7b6f34daf645a8b8a500f
Nicolas Picon <nicolas.picon@in-tact.fr>
2018-03-29 14:36:15 +0200

    Fix baby tests

--- app/src/test/java/fr/nicopico/hugo/domain/model/BabyTest.kt
+++ app/src/test/java/fr/nicopico/hugo/domain/model/BabyTest.kt
@@ -3,24 +3,25 @@ package fr.nicopico.hugo.domain.model
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.has
+import com.natpryce.hamkrest.startsWith
 import org.junit.Test
 
 class BabyTest {
     @Test
     fun keyForSimpleName() {
         val baby = Baby("Hugo")
-        assertThat(baby, has(Baby::key, equalTo("HUGO")))
+        assertThat(baby, has(Baby::key, startsWith("HUGO")))
     }
 
     @Test
     fun keyForComposedName1() {
         val baby = Baby("Anne-Cécile")
-        assertThat(baby, has(Baby::key, equalTo("ANNE_CECILE")))
+        assertThat(baby, has(Baby::key, startsWith("ANNE_CECILE")))
     }
 
     @Test
     fun keyForComposedName2() {
         val baby = Baby("Jean François")
-        assertThat(baby, has(Baby::key, equalTo("JEAN_FRANCOIS")))
+        assertThat(baby, has(Baby::key, startsWith("JEAN_FRANCOIS")))
     }
 }
