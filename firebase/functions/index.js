e7d72f42c4539e882df1d5cba9e2bf000bdc73b0
Nicolas Picon <nicopico.dev@gmail.com>
2018-04-16 00:04:32 +0200

    Cloud Functions: updateLastAction_DEV

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,17 @@
-const functions = require('firebase-functions');
+'use strict';
 
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-  // ...
-});
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.updateLastAction_DEV = functions.firestore
+    .document('dev_users/{userId}/babies/{baby}/timeline/{entry}')
+    .onCreate((snapshot, context) => {
+        const userId = context.params.userId;
+        return admin.firestore()
+            .doc(`dev_users/${userId}`)
+            .set(
+                { lastAction: context.timestamp }, 
+                { merge: true }
+            );
+    });
