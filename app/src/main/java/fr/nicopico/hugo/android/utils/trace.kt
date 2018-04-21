8c540b37c9989713240b9fe152a095a7e99b314e
Nicolas Picon <nicopico.dev@gmail.com>
2018-04-22 00:13:40 +0200

    Fix trace() swallowing return values

--- app/src/main/java/fr/nicopico/hugo/android/utils/trace.kt
+++ app/src/main/java/fr/nicopico/hugo/android/utils/trace.kt
@@ -3,10 +3,8 @@ package fr.nicopico.hugo.android.utils
 import com.google.firebase.perf.FirebasePerformance
 import com.google.firebase.perf.metrics.Trace
 
-inline fun trace(name: String, block: Trace.() -> Unit) {
-    FirebasePerformance.startTrace(name)
-            .apply(block)
-            .also {
-                it.stop()
-            }
+inline fun <T> trace(name: String, block: Trace.() -> T): T {
+    val trace = FirebasePerformance.startTrace(name)
+    val result = trace.let(block)
+    return result.also { trace.stop() }
 }
--- app/src/main/java/fr/nicopico/hugo/domain/model/Timeline.kt
+++ app/src/main/java/fr/nicopico/hugo/domain/model/Timeline.kt
@@ -1,6 +1,5 @@
 package fr.nicopico.hugo.domain.model
 
-import com.google.firebase.perf.FirebasePerformance
 import fr.nicopico.hugo.android.utils.trace
 import fr.nicopico.hugo.domain.utils.onlyDate
 import java.util.*
@@ -10,15 +9,15 @@ class Timeline(private val entries: List<Entry> = emptyList()) {
     val sections: List<Section>
 
     init {
-        val trace = FirebasePerformance.startTrace("Timeline.sections")
-        trace.incrementCounter("entries", entries.size.toLong())
-        sections = entries
-                .sortedByDescending { it.time }
-                .groupBy { it.time.onlyDate() }
-                .map { (date, entries) ->
-                    Section(date, entries)
-                }
-        trace.stop()
+        sections = trace("Timeline.sections") {
+            incrementCounter("entries", entries.size.toLong())
+            entries
+                    .sortedByDescending { it.time }
+                    .groupBy { it.time.onlyDate() }
+                    .map { (date, entries) ->
+                        Section(date, entries)
+                    }
+        }
     }
 
     operator fun plus(entry: Timeline.Entry) = Timeline(entries + entry)
@@ -40,7 +39,7 @@ class Timeline(private val entries: List<Entry> = emptyList()) {
     }
 
     data class Section(val time: Date, val entries: List<Entry>) {
-        val totalMilk by lazy {
+        val totalMilk: Int by lazy {
             trace("Section.totalMilk") {
                 incrementCounter("entries", entries.size.toLong())
                 entries.filter { it.type == CareType.FOOD }
